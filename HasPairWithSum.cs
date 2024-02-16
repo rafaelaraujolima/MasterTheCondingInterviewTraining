2842121707e17c8ace169ee4bd805eed4701f42d
Rafael Araujo de Lima <rafael.araujo.lima@outlook.com>
2024-02-15 22:18:49 -0400

    Answer With Space Complexity O(n) and Time Complexity O(n)

---
+++ HasPairWithSum.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterTheCodingInterviewTraining
+{
+    internal static class HasPairWithSum
+    {
+        public static bool CheckIfHasPairWithSum(int[] arr, int sum)
+        {
+            //assuming it's ordered
+            int low = 0;
+            int hi = arr.Length - 1;
+
+            while (low < hi)
+            {
+                if ((arr[low] + arr[hi]) == sum)
+                    return true;
+                else if ((arr[low] + arr[hi]) > sum)
+                    hi--;
+                else
+                    low++;
+            }
+            
+            return false;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Gave an arrays and a sum, verify if exists a pair that
+// the sum is equals to the value gave
+// Example:
+// [1, 2, 3, 9] and sum = 8 returns False
+// [1, 2, 4, 4] and sum = 8 returns True
+using MasterTheCodingInterviewTraining;
+
+internal class Teste
+{
+    static void Main(string[] args)
+    {
+        int[] array = [1, 2, 3, 9];
+        int sum = 8;
+
+        Console.WriteLine(HasPairWithSum.CheckIfHasPairWithSum(array, sum));
+    }
+}
