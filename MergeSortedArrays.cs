f203f82ebe9f60051e5bd8c6763f0154d8c05bb9
Rafael Araujo de Lima <rafael.araujo.lima@outlook.com>
2024-02-21 17:23:15 -0400

    First solution O(n+m)

---
+++ MergeSortedArrays.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterTheCodingInterviewTraining
+{
+    internal static class MergeSortedArrays
+    {
+        public static int[] MergeTheArrays(int[] arr1, int[] arr2)
+        {
+            if (arr1 != null && arr2 != null)
+            {
+                int[] mergedArray = new int[arr1.Length + arr2.Length];
+
+                int k = 0;
+
+                int idxArr1 = 0;
+                int idxArr2 = 0;
+
+                while (idxArr1 < arr1.Length && idxArr2 < arr2.Length)
+                {
+                    if (arr1[idxArr1] <= arr2[idxArr2])
+                    {
+                        mergedArray[k++] = arr1[idxArr1++];
+                    }
+                    else
+                    {
+                        mergedArray[k++] = arr2[idxArr2++];
+                    }
+                }
+                
+                while (idxArr1 < arr1.Length)
+                {
+                    mergedArray[k++] = arr1[idxArr1++];
+                }
+
+                while (idxArr2 < arr2.Length)
+                {
+                    mergedArray[k++] = arr2[idxArr2++];
+                }
+
+                return mergedArray;
+            }
+            return [];
+        }
+    }
+}
+
+
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// given two sorted arrays, merge then sorted
+// example:
+// input: [0,3,4,31], [4,6,30]
+// output: [0,3,4,4,6,30,31]
+using MasterTheCodingInterviewTraining;
+
+internal class Teste
+{
+    public static void Main(string[] args)
+    {
+        int[] arr1 = [0, 3, 4, 31];
+        //int[] arr2 = [4, 6, 30];
+        int[] arr2 = null;
+
+        Console.WriteLine(string.Join(" ", MergeSortedArrays.MergeTheArrays(arr1, arr2)));
+    }
+}
