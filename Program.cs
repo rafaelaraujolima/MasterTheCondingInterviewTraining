0e45541b7eb07857739a2bd367d92bdfc71cf908
Rafael Araujo de Lima <rafael.araujo.lima@outlook.com>
2024-02-27 20:09:01 -0400

    Solution Ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+using MasterTheCodingInterviewTraining;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(SolutionFirstRecurringCharacter.FirstRecurringCharacter([2, 3, 4, 5]));
+    }
+}
+
---
+++ SolutionFirstRecurringCharacter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterTheCodingInterviewTraining
+{
+    internal static class SolutionFirstRecurringCharacter
+    {
+        public static int FirstRecurringCharacter(int[] arr)
+        {
+            HashSet<int> nums = new HashSet<int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (nums.Contains(arr[i]))
+                    return arr[i];
+
+                nums.Add(arr[i]);
+            }
+
+            return int.MinValue;
+        }
+    }
+}
